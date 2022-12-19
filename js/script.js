0912438a8f74203da60a429d07763bc678a5aa8f
Vitor Pereira <vitor-pcruz@hotmail.com>
2022-12-19 15:01:29 -0300

    tabnav refatorado para classe

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 import ScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import Accordion from './modules/accordion.js';
-import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
@@ -10,6 +9,7 @@ import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import TabNav from './modules/tabnav.js';
 // #endregion
 
 // #region Criacao de classes e variaveis
@@ -18,11 +18,13 @@ const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 const list = '[data-anime="accordion"] dt';
 const accordion = new Accordion(list);
 
+const tabMenu = '[data-tab="menu"] li';
+const tabContent = '[data-tab="content"] section';
+const tabNav = new TabNav(tabMenu, tabContent);
 // #endregion
 
 // #region Iniciliazação de Funcoes
 initAnimacaoScroll();
-initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
@@ -33,4 +35,5 @@ initFetchBitcoin();
 
 scrollSuave.init();
 accordion.init();
+tabNav.init();
 // #endregion
