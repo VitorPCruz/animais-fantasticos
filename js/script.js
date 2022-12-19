7e782d8a86ab6650e8bce22419d38889b60cecf6
Vitor Pereira <vitor-pcruz@hotmail.com>
2022-12-19 18:51:53 -0300

    fetch-animais refatorado

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initFetchAnimais from './modules/fetch-animais.js';
+import fetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 import TabNav from './modules/tabnav.js';
 // #endregion
@@ -34,11 +34,12 @@ modal.init();
 const tooltips = new Tooltip('[data-tooltip]');
 tooltips.init();
 
+fetchAnimais('../animaisapi.json', '.numeros-grid');
+
 // #endregion
 
 initAnimacaoScroll();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
 initFetchBitcoin();
