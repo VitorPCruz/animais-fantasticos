eb52c73f9589affbd73cdbe33ce4059cf26d0253
Vitor Pereira <vitor-pcruz@hotmail.com>
2022-12-20 10:47:44 -0300

    script refatorado

--- js/script.js
+++ js/script.js
@@ -14,40 +14,32 @@ import fetchAnimais from './modules/fetch-animais.js';
 // #endregion
 
 // #region Criacao de classes e variaveis
-const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
-scrollSuave.init();
-
-const list = '[data-anime="accordion"] dt';
-const accordion = new Accordion(list);
-accordion.init();
-
-const tabMenu = '[data-tab="menu"] li';
-const tabContent = '[data-tab="content"] section';
-const tabNav = new TabNav(tabMenu, tabContent);
-tabNav.init();
-
-const botaoAbrir = '[data-modal="abrir"]';
-const botaoFechar = '[data-modal="fechar"]';
-const containerModal = '[data-modal="container"]';
-const modal = new Modal(botaoAbrir, botaoFechar, containerModal);
-modal.init();
-
-const tooltips = new Tooltip('[data-tooltip]');
-tooltips.init();
-
-const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
-scrollAnima.init();
-
-const dropdownMenu = new DropdownMenu('[data-dropdown]');
-dropdownMenu.init();
-
-const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
-menuMobile.init();
-
+const SCROLL_SUAVE = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+const ACCORDION = new Accordion('[data-anime="accordion"] dt');
+const TABNAV = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+const MODAL = new Modal(
+  '[data-modal="abrir"]',
+  '[data-modal="fechar"]',
+  '[data-modal="container"]',
+);
+const TOOL_TIPS = new Tooltip('[data-tooltip]');
+const SCROLL_ANIMA = new ScrollAnima('[data-anime="scroll"]');
+const DROPDOWN_MENU = new DropdownMenu('[data-dropdown]');
+const MENU_MOBILE = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 const FUNCIONAMENTO = new Funcionamento('[data-semana]');
-FUNCIONAMENTO.init();
 
 fetchAnimais('../animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+// #endregion
 
+// #region Inits
+SCROLL_SUAVE.init();
+ACCORDION.init();
+TABNAV.init();
+TOOL_TIPS.init();
+MODAL.init();
+SCROLL_ANIMA.init();
+DROPDOWN_MENU.init();
+MENU_MOBILE.init();
+FUNCIONAMENTO.init();
 // #endregion
